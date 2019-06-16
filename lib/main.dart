f202c1d1220af02dfda51e6e75b9aac5e4f44d29
Nagao-Y <b6196@oic.jp>
2019-06-16 23:11:49 +0900

    リストビューにListTileを動的に追加できるようにした

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: '買い物かごの合計金額',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -30,21 +29,19 @@ class MyHomePage extends StatefulWidget {
 
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+  final myController = TextEditingController();
+  //final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  //int _id;
+
+  List<Map<String,dynamic>> listItems = [
+    {"id":1, "goods":"goods1","price":"100","pieces":"0"},
+    {"id":2, "goods":"goods2","price":"100","pieces":"0"},
+    {"id":3, "goods":"goods3","price":"100","pieces":"0"},
+  ];
+  static int _thingCounter = 3;
   int _sum = 0;
   int _tax = 0;
-  static int _thingCounter = 1;
 
-  void _addSum(){
-
-  }
-
-
-  _createNewData(){//ビルドさせるデータを増やす
-    setState(() {//この中に変数の処理を書く
-      _thingCounter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,33 +51,85 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('$_thingCounter 合計：$_sum 円(消費税 $_tax ％込)'),
         backgroundColor: Colors.black,
       ),
-      body:_buildBuyList(),//中身の処理は別ウィジェットに任せる
+      body:ListView.builder(
+            itemBuilder: (BuildContext context,int i){
+              if(i.isOdd) return Divider();
+              final index = i ~/ 2;
+              if(index < _thingCounter){
+                return Container( padding: const EdgeInsets.all(8),
+                  child: Row(children: [
+
+                    //商品名
+                    Expanded(
+                      flex: 30,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: _thingCounter.toString() + "商品名",
+                        ),
+                        style: new TextStyle(
+                          fontSize: 18.0,              
+                        ),
+                        controller: myController,
+                      ),
+                    ),
+
+                    //単価
+                    Expanded(
+                      flex: 20,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: "単価",
+                          hintText: "0",
+                        ),
+                        style: new TextStyle(
+                          fontSize: 18.0,
+                        ),
+                        controller: myController                    
+                      ),
+                    ),
+
+                    //数量
+                    Expanded(
+                            flex: 10,
+                            child: TextField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                labelText: "数量",
+                                hintText: "0",
+                              ),
+                              style: new TextStyle(
+                                fontSize: 18.0,
+                              ),
+                              controller: myController
+                            ),
+                          ),
+
+                  ],),
+                ); 
+              }
+            }
+          ),
       
       floatingActionButton: FloatingActionButton(
-        onPressed: _createNewData,
+        onPressed: _createNewList(),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
   
-  Widget _buildBuyList() {//メインコンテンツ
-    return ListView(
-      children: List.generate(_thingCounter, (int index){
-        return Card(
-          child: ListTile(
-            title: Text('$index:'),
-          ),
-        );
-        return Card(
-          child: TextField(
-            decoration: const InputDecoration(
-              labelText: '商品名'
-            ),
-          ),
-        );
-      }),
-    );
+  _createNewList(){
+    setState(() {//この中に変数の処理を書く
+      _thingCounter++;
+    });
   }
-  
+
+  void  _changeItemData(int id,String goodsName,int price,int pieces){//数値を反映させる
+    setState(() {//この中に変数の処理を書く
+      listItems.add({"id":_thingCounter.toString(),"goods":goodsName,"price":price,"pieces":pieces});
+    });
+  }
+
 }
