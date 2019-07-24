00adb9be2999b721872b3c20c7ff50c19267e7cf
yuki.nagao <shivarea0914@gmail.com>
2019-07-24 14:53:21 +0900

    スライドでリスト追加に変更

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _sum = 0;
   int _tax = 0;
 
+
   @override
   Widget build(BuildContext context) {
 
@@ -41,10 +42,12 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('$_thingCounter 合計：$_sum 円(消費税 $_tax ％込)'),
         backgroundColor: Colors.black54,
       ),
-      body: new ListView(
-        children: new List.generate(_thingCounter, (int index){
-          index += 1;
-          return new Container(
+      body: RefreshIndicator(
+        child: ListView.builder(
+          itemCount: _thingCounter,
+          itemBuilder: (BuildContext context,int index){
+            index += 1;
+            return new Container(
             child: Row(
               children: [
                     Expanded(
@@ -97,14 +100,86 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           );
-        },),
+          },
+        ),
+        // child:new ListView(
+        //   new List.generate(_thingCounter, (int index){
+        //   index += 1;
+        //   return new Container(
+        //     child: Row(
+        //       children: [
+        //             Expanded(
+        //               flex: 3,
+        //               child: Text("#$index"),
+        //             ),
+
+        //             //商品名
+        //             Expanded(
+        //               flex: 30,
+        //               child: TextField(
+        //                 decoration: InputDecoration(
+        //                   labelText: "商品名",
+        //                 ),
+        //                 style: new TextStyle(
+        //                   fontSize: 18.0,
+        //                 ),
+        //               ),
+        //             ),
+
+        //             //単価
+        //             Expanded(
+        //               flex: 20,
+        //               child: TextField(
+        //                 keyboardType: TextInputType.number,
+        //                 decoration: InputDecoration(
+        //                   labelText: "単価",
+        //                   hintText: "0",
+        //                 ),
+        //                 style: new TextStyle(
+        //                   fontSize: 18.0,
+        //                 ),
+        //               ),
+        //             ),
+
+        //             //数量
+        //             Expanded(
+        //               flex: 10,
+        //               child: TextField(
+        //                 keyboardType: TextInputType.number,
+        //                 decoration: InputDecoration(
+        //                   labelText: "数量",
+        //                   hintText: "0",
+        //                 ),
+        //                 style: new TextStyle(
+        //                   fontSize: 18.0,
+        //                 ),
+        //               ),
+        //             ),
+        //       ],
+        //     ),
+        //   );
+        //   },),
+        // ),
+        onRefresh: _refresh,
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _createNewList,
-        child: Text("plus"),
-      ),
+      
+      // floatingActionButton: new FloatingActionButton(
+      //   onPressed: _createNewList,
+      //   child: Text("plus"),
+      // ),
     );
+
+
+
   }
+
+    Future<void> _refresh() {
+      return Future.sync(() {
+        setState(() {
+          _thingCounter++;
+        });
+      });
+    }
 
   _createNewList(){
     setState(() {//この中に変数の処理を書く
